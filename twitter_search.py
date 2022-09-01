97528264d6928b6df14705954d5a321817b91041
Davidelvis <komboelvis08@gmail.com>
2022-09-01 21:28:55 +0300

    Python environment

---
+++ twitter_search.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+
+search_url = "https://api.twitter.com/2/tweets/search/recent"
+
+
+# Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
+# expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
+query_params = {'query': '(from:twitterdev -is:retweet) OR #twitterdev','tweet.fields': 'author_id,public_metrics'}
